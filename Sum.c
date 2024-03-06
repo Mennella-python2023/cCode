94199775cb06e93d475df92715a58be435258061
Mennella-python2023 <mennella.maddalena@itisgalileiroma.it>
2024-03-06 14:26:28 +0100

    code

---
+++ Sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2, somma;
+
+    printf("Inserisci il primo numero: ");
+    scanf("%d, &numero1");
+ 
+    printf("Inserisci il secondo numero: ");
+    scanf("%d, &numero2");
+
+    somma = numero1 + numero2;
+
+    printf("La somma dei due numeri è: %d\n", somma);
+
+    return 0;
+}
